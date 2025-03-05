7daf587fb111b3596e301a47920fb101242af630
Jones R. Bezerra <jones.rom22@gmail.com>
2025-03-05 11:19:22 -0300

    Creating and retrieving users

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
 from http import HTTPStatus
 
-from fast_zero.schemas import Message
+from fastapi import FastAPI
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
@@ -9,3 +10,19 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+database = []
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,30 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_o_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_ok_e_o_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user(client, user_data):
+    response = client.post('/users/', json={**user_data})
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': user_data['username'],
+        'email': user_data['email'],
+    }
+
+
+def test_read_users(client, user_data):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': user_data['username'],
+                'email': user_data['email'],
+            }
+        ]
+    }
