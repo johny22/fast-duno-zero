e630147809a3e07eebaea798e2125af0a395bed7
Jones R. Bezerra <jones.rom22@gmail.com>
2025-03-25 12:15:32 -0300

    Persisting data on database

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_o_ola_mundo(client):
     response = client.get('/')
@@ -12,35 +14,62 @@ def test_create_user(client, user_data):
     response = client.post('/users/', json={**user_data})
     assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
+        'id': 1,
         'username': user_data['username'],
         'email': user_data['email'],
     }
 
 
-def test_read_users(client, user_data):
+def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': user_data['username'],
-                'email': user_data['email'],
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client, user_data):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user, user_data):
     user_data['email'] = 'jones@mail.com'
     response = client.put('/users/1', json=user_data)
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
+        'id': 1,
         'username': user_data['username'],
         'email': user_data['email'],
     }
 
 
-def test_delete_user(client, user_data):
+def test_update_integrity_error(client, user):
+    client.post(
+        '/users',
+        json={
+            'username': 'fausto',
+            'email': 'fausto@example.com',
+            'password': 'secret',
+        },
+    )
+
+    response_update = client.put(
+        f'/users/{user.id}',
+        json={
+            'username': 'fausto',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response_update.status_code == HTTPStatus.CONFLICT
+    assert response_update.json() == {
+        'detail': 'Username or Email already exists'
+    }
+
+
+def test_delete_user(client, user):
     response = client.delete('/users/1')
     assert response.json() == {'message': 'User deleted successfully!'}
